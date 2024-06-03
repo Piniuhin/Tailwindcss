1cbcd605720e4043aec88cfdda2cdfb16d93d329
Alexander Piniuhin <85253534+Piniuhin@users.noreply.github.com>
2024-06-03 15:04:01 +0200

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "20px",
+        xl: "25px", // extra large devices, large laptops, desktops
+        xxl: "30px", // xxl devices, large desktops
+        "2xl": "35px", // Full HD screens
+        "3xl": "40px", // QHD screens
+        "4xl": "45px", // 4K screens
+        "5xl": "50px", // covers everything above
+      },
+    },
     extend: {},
   },
   plugins: [],
